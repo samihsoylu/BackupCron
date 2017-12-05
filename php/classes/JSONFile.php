ba982dc2302497f8bc200bd1ec7cc733bb413414
Samih Soylu <samih@nivano.nl>
2017-12-05 21:33:36 +0100

    Added JSON file read/write class

---
+++ php/classes/JSONFile.php
@@ -0,0 +1,47 @@
+<?php
+class JSONFile {
+
+  /*
+  * Reads JSON file and returns the data
+  *
+  * @param $file - File location to open & read
+  * @param $openMode - File opening mode
+  */
+  public static function read($file, $openMode='r') {
+
+    # Opens
+    $f = fopen($file, $openMode);
+
+    # Read total file. If file is empty, read 1 to not have a warning.
+    $JSONData = fread($f, max(filesize($file), 1));
+
+    # Decodes data
+    $arrayData = JSON_decode($JSONData, true);
+
+    # Returns it
+    return $arrayData;
+
+  }
+
+  /*
+  * Writes in to JSON file and closes file
+  *
+  * @string $file - File location to open & write
+  *
+  * @array $data - The data that will be write n in to the file.
+  * @string $openMode - The mode this file will be opened.
+  */
+  public static function write($file, $data, $openMode='w+') {
+
+    # Opens file
+    $f = fopen($file, $openMode);
+
+    # Encodes and puts in data
+    fputs($f, JSON_encode($data, JSON_PRETTY_PRINT));
+
+    # Closes file
+    fclose($f);
+
+  } // End of write()
+
+}
