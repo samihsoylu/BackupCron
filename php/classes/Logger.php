74250ca3f3a1ec0f67a21f4a869f7a470bd8f309
Samih Soylu <samih@nivano.nl>
2017-12-05 21:33:23 +0100

    Added logger class

---
+++ php/classes/Logger.php
@@ -0,0 +1,39 @@
+<?php
+class Logger {
+
+  /*
+  * Saves an error message to file. Creates a .log file if it does not exist
+  *
+  * @param $errorFile - Name of the file
+  * @param $errorMessage - Message to put in to file.
+  * @return void
+  */
+  public static function SaveError($errorName, $errorMessage, $init) {
+
+    # Where the error will be saved
+    $errorFile = BASE_PATH.'logs/'.$errorName.'.log';
+
+    # Before the error message, date is displayed
+    $date_of_error = "[".date('d M Y H:i:s')."] :: ";
+
+    # Creates log if it does not exist
+    if(!file_exists($errorFile)) {
+      $openedFile = fopen($errorFile, "w");
+      fclose($openedFile);
+    }
+
+    # Reads error log
+    $errorFileData = file_get_contents($errorFile);
+
+    # Amendments to variable
+    $errorFileData .= $date_of_error . $errorMessage . "\n\n";
+
+    # Writes to file
+    file_put_contents($errorFile, $errorFileData);
+
+    # Send an email to an admin
+    @mail($init['debug']['email'], $init['debug']['apptitle'].': Error logger', $errorMessage);
+
+  }
+
+}
